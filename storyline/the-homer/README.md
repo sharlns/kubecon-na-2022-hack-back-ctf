46f36508a4f76da076213c576816e5f7394957a1
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2022-10-24 16:12:43 +0100

    feat - the-homer-config
    - create script to generate kubectl config file
    - update README for the-homer storyline

---
+++ storyline/the-homer/README.md
@@ -0,0 +1,13 @@
+# The Homer
+
+Foobar Taxi's are proud to announce that they will only transport you in _The Homer_!
+
+![Homer Simpson in The Homer](./the-homer.gif)
+
+## Information
+
+We monitor our drivers and passengers via our Role Based Automotive Control (RBAC) to allow you to get your homer.
+
+## Updates
+
+Foobar Taxi's are please to announce that passengers can now update their rides thanks to our update Role Based Automotive Control (RBAC) policies, why not give it a try and pivot into your next ride!
---
+++ storyline/the-homer/kubepwned-config.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+clusterName=gke_natalia-testing-288908_europe-central2-a_kubecon-na-ctf-1
+namespace=the-homer
+serviceAccount=the-homer-sa
+newfile=config.pwned
+
+server=$(kubectl config view --minify --raw -o jsonpath='{.clusters[].cluster.server}')
+secretName=$(kubectl --namespace $namespace get serviceAccount $serviceAccount -o jsonpath='{.secrets[0].name}')
+ca=$(kubectl --namespace $namespace get secret/$secretName -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl --namespace $namespace get secret/$secretName -o jsonpath='{.data.token}' | base64 --decode)
+
+echo "
+---
+apiVersion: v1
+kind: Config
+clusters:
+  - name: ${clusterName}
+    cluster:
+      certificate-authority-data: ${ca}
+      server: ${server}
+contexts:
+  - name: ${serviceAccount}@${clusterName}
+    context:
+      cluster: ${clusterName}
+      namespace: ${namespace}
+      user: ${serviceAccount}
+users:
+  - name: ${serviceAccount}
+    user:
+      token: ${token}
+current-context: ${serviceAccount}@${clusterName}
+" >> ${newfile}
