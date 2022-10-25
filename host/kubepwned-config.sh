55e40300d7003e0d63785b9ac80d7537696c0627
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2022-10-25 21:10:06 +0100

    fix: remove pwned from filename

--- host/kubepwned-config.sh
+++ host/kubepwned-config.sh
@@ -3,7 +3,7 @@
 clusterName=gke_natalia-testing-288908_europe-central2-a_kubecon-na-ctf-1
 namespace=the-homer
 serviceAccount=the-homer-sa
-newfile=config.pwned
+newfile=config
 
 server=$(kubectl config view --minify --raw -o jsonpath='{.clusters[].cluster.server}')
 secretName=$(kubectl --namespace $namespace get serviceAccount $serviceAccount -o jsonpath='{.secrets[0].name}')
